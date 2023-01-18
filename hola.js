eafb7357611547ed36ab7d289f66ce2de4d04a68
Julian <michaeljulian50@gmail.com>
2023-01-17 22:53:17 -0500

    limpio codigo

--- hola.js
+++ hola.js
@@ -7,38 +7,3 @@ const messageAlert = (text) =>{
     });
 }
 
-const messageAlert2 = (text) => {
-    Swal.fire({
-        title: "Congratulations",
-        text: `${text}.`,
-        icon: "success",
-        confirmButtonColor: "blue",
-    });
-}
-
-const messageAlert3 = (text) => {
-    Swal.fire({
-        title: "Congratulations",
-        text: `${text}.`,
-        icon: "success",
-        confirmButtonColor: "blue",
-    });
-}
-
-const messageAlert4 = (text) => {
-    Swal.fire({
-        title: "Congratulations",
-        text: `${text}.`,
-        icon: "success",
-        confirmButtonColor: "blue",
-    });
-}
-
-const messageAlert5 = (text) => {
-    Swal.fire({
-        title: "Congratulations",
-        text: `${text}.`,
-        icon: "success",
-        confirmButtonColor: "blue",
-    });
-}
