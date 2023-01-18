ffd6565e47632ad447a76e553c2f8a9be347d226
Julian <michaeljulian50@gmail.com>
2023-01-17 22:32:09 -0500

    a

---
+++ hola.js
@@ -0,0 +1,26 @@
+const messageAlert = (text) =>{
+    Swal.fire({
+        title: "Congratulations",
+        text: `${text}.`,
+        icon: "success",
+        confirmButtonColor: "blue",
+    });
+}
+
+const messageAlert2 = (text) => {
+    Swal.fire({
+        title: "Congratulations",
+        text: `${text}.`,
+        icon: "success",
+        confirmButtonColor: "blue",
+    });
+}
+
+const messageAlert3 = (text) => {
+    Swal.fire({
+        title: "Congratulations",
+        text: `${text}.`,
+        icon: "success",
+        confirmButtonColor: "blue",
+    });
+}
